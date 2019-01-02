a5fdbc2f4cd5d5a74448de41885879602f2b0c6e
Simon Eddy <simon@simoneddy.com.au>
2019-01-02 14:13:38 +1100

    updates

--- src/is_sequential.php
+++ src/is_sequential.php
@@ -1,9 +1,23 @@
 <?php
 if (!function_exists('is_sequential')) {
+
+    /**
+     * Check if an array should be considered Sequential.
+     * 
+     * This function will return true only if:
+     *  - A key with the value of 0 is present,
+     *  - Only integer keys are present.
+     * 
+     * If $array is empty or does not fulfill the above requirements this
+     * function will return false.
+     *
+     * @param array $array
+     * @return boolean
+     */
     function is_sequential(array $array)
     {
         if (isset($array[0])
-            && is_int(implode('', array_keys($array)))
+            && !preg_match('/\D/', implode('', array_keys($array)))
         ) {
             return true;
         }
